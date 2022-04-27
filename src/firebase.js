3c504791721c8c70ae4286d7355af1d1356aadd8
Tarek Jaman Labu <tarekjamanlabu515@gmail.com>
2022-04-27 15:53:14 +0600

    project finished

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDSMHeWGAgIOVWbmh9YC8i5P3QpWbdVRdw',
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: 'disney-clone-b9b42.firebaseapp.com',
   projectId: 'disney-clone-b9b42',
   storageBucket: 'disney-clone-b9b42.appspot.com',
-  messagingSenderId: '226771558402',
-  appId: '1:226771558402:web:d9561a0260711e5fb7c71b',
-  measurementId: 'G-ZE7KLP9M6T',
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
 }
 
 // Initialize firebase
